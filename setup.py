fa8495bc380b25d1bfb243288c4fcffb1bad41d2
Jonas Baumann <jone@jone.ch>
2019-08-20 13:49:41 +0200

    Bootstrap package.

---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = '1.0.0.dev0'
+
+
+tests_require = []
+
+
+setup(name='opengever.apiclient',
+      version=version,
+      description='API Client for OneGov GEVER',
+
+      long_description=open('README.rst').read() + '\n'
+      + open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+
+      keywords='onegov gever opengever api client',
+      author='4teamwork AG',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/opengever.apiclient',
+
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['opengever', ],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+          'setuptools',
+      ],
+
+      tests_require=tests_require,
+      extras_require={'tests': tests_require})
