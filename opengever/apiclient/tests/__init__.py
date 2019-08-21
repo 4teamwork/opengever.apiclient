dd668e3634a9724cf511b23f886129d6723fd976
Jonas Baumann <jone@jone.ch>
2019-08-21 13:56:53 +0200

    Implement testserver isolation.

--- opengever/apiclient/tests/__init__.py
+++ opengever/apiclient/tests/__init__.py
@@ -5,6 +5,7 @@ import os
 import shutil
 import tempfile
 import unittest
+import xmlrpc.client
 
 from ..keys import KeyRegistry
 from ..session import GEVERSession
@@ -14,6 +15,8 @@ from ..utils import singleton
 PACKAGE_ROOT = (Path(__file__) / '..' / '..' / '..' / '..').resolve()
 TESTS_DIR = (Path(__file__) / '..').resolve()
 PLONE_URL = os.environ.get('TESTSERVER_PLONE_URL', 'http://localhost:55001/plone/')
+XMLRPC_URL = os.environ.get('TESTSERVER_XMLRPC_URL', 'http://localhost:55002')
+TESTSERVER_PROXY = xmlrpc.client.ServerProxy(XMLRPC_URL)
 
 
 class TestCase(unittest.TestCase):
@@ -25,6 +28,7 @@ class TestCase(unittest.TestCase):
     def setUp(self):
         KeyRegistry.reset()
         GEVERSession.clear()
+        self.testserver_isolation()
 
     @contextmanager
     def env(self, **env):
@@ -49,6 +53,22 @@ class TestCase(unittest.TestCase):
         missing = Missing()
         prepared_got = {key: got.get(key, missing) for key in expected}
         self.assertEqual(expected, prepared_got, msg)
+
+    @staticmethod
+    def testserver_isolation():
+        """Apply testserver isolation in order to prepare for the next test.
+        This includes setting up the testserver be sending zodb_setup.
+        This is a robust implementation which expects that sometimes the
+        testserver is not yet torn down properly (e.g. when keeping the testserver
+        running in local development). Therefore we try to tear down first.
+        """
+        for attempt in range(3):
+            try:
+                TESTSERVER_PROXY.zodb_teardown()
+            except xmlrpc.client.Fault:
+                break  # already torn down enough
+
+        TESTSERVER_PROXY.zodb_setup()
 
 
 @singleton
